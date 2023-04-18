85cf2c14ef7387d7d3efad98385114629956b3b0
olmetaprogrammer <128503773+olmetaprogrammer@users.noreply.github.com>
2023-04-18 14:13:25 +0200

    debug

--- Rules.py
+++ Rules.py
@@ -34,7 +34,7 @@ class troupes ():
         self.territoire = territoire
         
 #Les missions seront en fonction du nombre de joueurs et du temps approximatifs de la partie à laquelle ils veulent jouer
-# exemple : si il  y a 3 joueurs qui veulent jouer 10 minutes : ils doivent conquérir 15 territoires avec au moins 2 trouês par territoires        
+# exemple : si il  y a 3 joueurs qui veulent jouer 10 minutes : ils doivent conquérir 15 territoires avec au moins 2 trouês par territoires
 class mission () : 
     def __init__(self, zone, joueur, nom, description,nb_territoires_pour_mission,etat,nb_troupes_sur_territoires):
         self.nom = nom
@@ -45,14 +45,14 @@ class mission () :
         self.etat = etat     #l'etat est un attribut pour distinguer lorsque la mission
                              #est accomplie
     def mission_accomplie (joueur,mission):
-        for territoire_occupe in joueur['territoires_que_le_joueur_occupe'] : 
-            if joueur['nb_territoires_du_joueurs']>=mission['nb_territoires_pour_mission'] and territoire_occupe['nombre_troupes']>= mission['nb_troupes_sur_territoires'] : 
+        for territoire_occupe in joueur['territoires_que_le_joueur_occupe'] :
+            if joueur['nb_territoires_du_joueurs']>=mission['nb_territoires_pour_mission'] and territoire_occupe['nombre_troupes']>= mission['nb_troupes_sur_territoires'] :
                 mission['etat'] = 'accomplie'
 
                 #pour e+repérer si un joueur a gagné la partie on doit vérifier que toutes les missions qu'il a sont terminées (à lier à la classe joueur)
 
 
-#class bonus() : 
+#class bonus() :
 #a faire quand les autres trucs marcheront
 
 
@@ -71,22 +71,22 @@ def attaque(territoire_attaquant, territoire_attaque):
     scores_attaquant = []
     scores_attaque = []
     nb_des_a_comparer = territoire_attaquant['nombre_troupes'] - territoire_attaque['nombre_troupes']
-    for i in range (territoire_attaquant['nombre_troupes']) : 
+    for i in range (territoire_attaquant['nombre_troupes']) :
         scores_attaquant.append(des())
-    
-    for i in range (territoire_attaque['nombre_troupes']) : 
+
+    for i in range (territoire_attaque['nombre_troupes']) :
         scores_attaque.append(des())
     scores_attaquant = tri_fusion(scores_attaquant)
     scores_attaque = tri_fusion(scores_attaque)
     gagnant = 0
     while i <= nb_des_a_comparer and gagnant == 0 :
-        if scores_attaquant[i]<=scores_attaque[i] : 
+        if scores_attaquant[i]<=scores_attaque[i] :
             territoire_attaquant['nombre_troupes']-=1
             i+=1
-        if scores_attaquant[i]>scores_attaque[i] : 
+        if scores_attaquant[i]>scores_attaque[i] :
             territoire_attaque['nombre_troupes']-=1
             i+=1
-        if territoire_attaquant['nombre_troupes']== 0 : 
+        if territoire_attaquant['nombre_troupes']== 0 :
             gagnant = 1
             print('Défenseur, vous vous êtes bien défendu, il ne reste plus de troupes à votre ennemi, déplacez vos troupes pour occuper son territoire!')
         if territoire_attaque['nombre_troupes'] == 0 :
@@ -109,8 +109,8 @@ def tri_fusion(liste):
 def fusion_triee(liste1, liste2):
     resultat = []
     i = j = 0
-    while i< len(liste1) and j < len(liste2) : 
-        if liste1[i] < liste2[j] : 
+    while i< len(liste1) and j < len(liste2) :
+        if liste1[i] < liste2[j] :
             resultat.append(liste1[i])
             i+=1
         else :
@@ -119,16 +119,16 @@ def fusion_triee(liste1, liste2):
     resultat.extend(liste1[i:] or liste2[j:])
     return resultat
 
-    
+
 
 
 def transfert_troupes (territoire_de_depart,territoire_arrivee,nb_troupes_a_transferer):
-    if nb_troupes_a_transferer<=0 : 
+    if nb_troupes_a_transferer<=0 :
         print('Veuillez donner un nombre strictement positif de troupes à transférer')
-    if nb_troupes_a_transferer < territoire_de_depart : 
+    if nb_troupes_a_transferer < territoire_de_depart :
         territoire_de_depart['nombre_troupes'] = territoire_de_depart['nombre_troupes'] - nb_troupes_a_transferer
         territoire_arrivee['nombre_troupes'] += nb_troupes_a_transferer
-    else : 
+    else :
         print('Vous ne pouvez pas transférer autant de troupes !!!!')    # il faut rajouter la condition de proximité lorsque l'on est dans la partie après le placement
 
 
@@ -156,54 +156,33 @@ def placement_de_tous_les_joueurs(liste_joueurs):
 
 
 
-def placement_initial(joueur,nb_troupes_a_placer,nb_territoire_a_occuper, liste_territoires_restant): 
+def placement_initial(joueur,nb_troupes_a_placer,nb_territoire_a_occuper, liste_territoires_restant):
     joueur['nb_troupes']= nb_troupes_a_placer          #depend du nombre de joueurs : l'info sera à mettre sur un fichier json que l'on lira
     i=0
     territoires_occupés_par_le_joueur=[]
     nombre_de_troupes_qu_il_reste_a_placer = nb_troupes_a_placer
 
 
-    while i <=nb_territoire_a_occuper : 
+    while i <=nb_territoire_a_occuper :
         a=random.randint(0,(len(liste_territoires)-1))                      #  On attribue une liste de territoires a occuper par le joueur
         territoires_occupés_par_le_joueur.append(liste_territoires_restant[a])
         liste_territoires_restant.remove(liste_territoires_restant[a])
         i+=1
 
-    for territoire in territoires_occupés_par_le_joueur : 
+    for territoire in territoires_occupés_par_le_joueur :
+        territoire['nombre_troupes'] = 1                            #On place une troupe par territoire
+        nombre_de_troupes_qu_il_reste_a_placer-=1
+
+    while nombre_de_troupes_qu_il_reste_a_placer>0 :
+        territoire_ou_il_faut_ajouter_des_troupes = territoires_occupés_par_le_joueur[(input("Choisissez l'indice du territoire ou il y a trop de troupes"))]
+        nombre_de_troupes_a_ajouter = input("Combien de troupes voulez-vous ajouter a ce territoire ?")
+        if nombre_de_troupes_a_ajouter>nombre_de_troupes_qu_il_reste_a_placer :                      #Le joueur ajoute des troupes sur les territoires qu'il
+            print("Il ne vous reste pas assez de troupes !!")
+        else :
+            territoire_ou_il_faut_ajouter_des_troupes['nombre_troupes'] += nombre_de_troupes_a_ajouter
+            nombre_de_troupes_qu_il_reste_a_placer-=nombre_de_troupes_a_ajouter
 
 
-        if nombre_de_troupes_qu_il_reste_a_placer > 0 : 
-
-            nb_troupes_a_placer_sur_ce_territoire = input("Combien du troupes voulez-vous placer sur ce territoire ?")
-            if nb_troupes_a_placer_sur_ce_territoire <=0 : 
-                print("Vous devez avoir des troupes sur chaque territoire !!")
-                nb_troupes_a_placer_sur_ce_territoire = input("Combien du troupes voulez-vous placer sur ce territoire ?")
-            elif nb_troupes_a_placer_sur_ce_territoire > nombre_de_troupes_qu_il_reste_a_placer : 
-                print("Vous ne pouvez pas placer autant de troupes !! ")
-                nb_troupes_a_placer_sur_ce_territoire = input("Combien du troupes voulez-vous placer sur ce territoire ?")
-            elif nb_troupes_a_placer_sur_ce_territoire >0 : 
-                territoire['nommbre_troupes'] = nb_troupes_a_placer_sur_ce_territoire
-                nombre_de_troupes_qu_il_reste_a_placer-=nb_troupes_a_placer_sur_ce_territoire
-
-
-
-        if nombre_de_troupes_qu_il_reste_a_placer == 0 :
-            print("Tous vos territoires ne sont pas occupés par des troupes")
-            territoire_ou_il_y_a_trop_de_troupes = territoires_occupés_par_le_joueur(input("Choisissez l'indice du territoire ou il y a trop de troupes"))
-            if territoire_ou_il_y_a_trop_de_troupes['nombre_troupes'] == 0 : 
-                print("Vous n'avez pas encore placé de troupes sur ce territoires")
-                territoire_ou_il_y_a_trop_de_troupes = territoires_occupés_par_le_joueur(input("Choisissez l'indice du territoire ou il y a trop de troupes"))
-            elif territoire_ou_il_y_a_trop_de_troupes['nombre_troupes'] == 1 : 
-                print('Il n y a qu une troupe sur ce territoire, vous ne pouvez pas la transférer')
-                territoire_ou_il_y_a_trop_de_troupes = territoires_occupés_par_le_joueur(input("Choisissez l'indice du territoire ou il y a trop de troupes"))
-            elif territoire_ou_il_y_a_trop_de_troupes['nombre_troupes']>=2 : 
-                nb_troupes_a_transferer = input ("Combien de troupes voulez-vous transférer ? ")
-                if nb_troupes_a_transferer >= territoire_ou_il_y_a_trop_de_troupes['nombre_troupes'] :
-                    print("Vous ne pouvez pas transférer autant de troupes, sinon vous laisserez un territoire complètement sans défense")
-                    nb_troupes_a_transferer = input ("Combien de troupes voulez-vous transférer ? ")
-                elif nb_troupes_a_transferer < territoire_ou_il_y_a_trop_de_troupes['nombre_troupes'] : 
-                    transfert_troupes (territoire_ou_il_y_a_trop_de_troupes,territoire,nb_troupes_a_transferer)
-                
 
 
     # il faut que le joueur tire des territoires au hasard où il placera ses troupes comme il le souhaite avec toujours au minimum une troupe sur chaque territoire occupé

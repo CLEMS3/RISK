316909671f0316853d2d5cd9d18330123cbca5a2
AntoinePscl <antoine.pascal03@gmail.com>
2023-05-02 16:58:00 +0200

    premier essai

--- main.py
+++ main.py
@@ -311,7 +311,7 @@ class MainMenu :
             print(self.OUT)
             if len(self.OUT) == int(self.NbrJoueur.get()):
                 # create the window
-                window_pg = carte_final.PygameWindow((640, 480), self.liste_joueurs)
+                window_pg = carte_final.PygameWindow((self.WIDTH, self.HEIGHT), self.liste_joueurs)
 
                 # run the main loop
                 window_pg.main_loop()

fde175a672c0a27e2da2e73148440bea73b8cc3e
vitto4 <128498605+vitto4@users.noreply.github.com>
2023-05-02 20:23:22 +0200

    help

---
+++ carte_finale.py
@@ -0,0 +1,273 @@
+
+                    running = False
+
+                #différentes vus
+
+                                    print(self.select)dhkwgbkdbfgdgdf
+                                    dsgdfsiogosdfhgodfighdfisugh ods fsdg f 
+                                    fxghdfhgf
+                            except IndexError:dhkwgbkdbfgdgdf
+                            dsgdfsiogosdfhgodfighdfisugh ods fsdg f 
+                            fxghdfhgf
+                                passdhkwgbkdbfgdgdf
+                                dsgdfsiogosdfhgodfighdfisugh ods fsdg f 
+                                fxghdfhgf
+                    if event.type == pygame.KEYDOWN:qkdsbkqjsbdjkqsdhkwgbkdbfgdgdf
+                    dsgdfsiogosdfhgodfighdfisugh ods fsdg f 
+                    fxghdfhgfbdqksjdbqskjdbqskbdqskdjbqskdjbqskjdbqskdjbqksjdbqskdbqksdjbqskjbdqskdbqskdjbqskdjbq kjqbdkj qbdkbqskd bjqdkjbqkd bjqkdsj bqkjdbqjkdb qkj bdqksjbd qkdb qkjbd qksjdbqkjbs dqksjdqkjbdqskbqskdbfjgzeg  gzieru zueigrsdfkuog zer gjkwfsjqghsfuqy  quygqsfhvqlkjsbh lzyfluvyffuykvq jfhjvsf jhdfkqjzejyg uyfqukvzef jhhfvqkzevf hjvdhsdfsqz eluqgefgqiuzegf ukjbj sd gdfjgb sdj jskdgb sdjkfsjdbgizeugt ip lgskvkhilsghilgu qlzfifg
+dhkwgbkdbfgdgdf
+dsgdfsiogosdfhgodfighdfisugh ods fsdg f 
+fxghdfhgf
+dhkwgbkdbfgdgdf
+dsgdfsiogosdfhgodfighdfisugh ods fsdg f 
+fxghdfhgf
+dhkwgbkdbfgdgdf
+dsgdfsiogosdfhgodfighdfisugh ods fsdg f 
+fxghdfhgf
+dhkwgbkdbfgdgdf
+dsgdfsiogosdfhgodfighdfisugh ods fsdg f 
+fxghdfhgf
+                    dufghieh tiuthiezugtiezurrt rzt dhkwgbkdbfgdgdf
+                    dsgdfsiogosdfhgodfighdfisugh ods fsdg f 
+                    fxghdfhgf
+                    ze tezouth ezilruth ezrt dhkwgbkdbfgdgdf
+                    dsgdfsiogosdfhgodfighdfisugh ods fsdg f 
+                    fxghdfhgf
+                    et ozemth eilh ilt hhbxklg hselbktg smlgsl moaedhkwgbkdbfgdgdf
+                    dsgdfsiogosdfhgodfighdfisugh ods fsdg f 
+                    fxghdfhgftzj;fbdfnhzqfgeilr ze  e ei il diluger 
+                    egieshr gpidusrhg swg esrdhkwgbkdbfgdgdf
+                    dsgdfsiogosdfhgodfighdfisugh ods fsdg f 
+                    fxghdfhgf
+                    mg sùhg sedhkwgbkdbfgdgdf
+                    dsgdfsiogosdfhgodfighdfisugh ods fsdg f 
+                    fxghdfhgf
+                    ùg kesràv soe qr$dhkwgbkdbfgdgdf
+                    dsgdfsiogosdfhgodfighdfisugh ods fsdg f 
+                    fxghdfhgf
+                    l'-'(lgdhkwgbkdbfgdgdf
+                    dsgdfsiogosdfhgodfighdfisugh ods fsdg f 
+                    fxghdfhgf
+                    emtdhkwgbkdbfgdgdf
+                    dsgdfsiogosdfhgodfighdfisugh ods fsdg f 
+                    fxghdfhgf
+                    ^tu dhkwgbkdbfgdgdf
+                    dsgdfsiogosdfhgodfighdfisugh ods fsdg f 
+                    fxghdfhgf
+                    qsT s Sjs*r=ày udhkwgbkdbfgdgdf
+                    dsgdfsiogosdfhgodfighdfisugh ods fsdg f 
+                    fxghdfhgf
+                        if event.key == pygame.K_m:qkdsbkqjsbdjkqsbdhkwgbkdbfgdgdf
+                        dsgdfsiogosdfhgodfighdfisugh ods fsdg f 
+                        fxghdfhgfdqksjdbqskjdbqskbdqskdjbqskdjbqskjdbqskdjbqksjdbqskdbqksdjbqskjbdqskdbqskdjbqskdjbq kjqbdkj qbdkbqskd bjqdkjbqkd bjqkdsj bqkjdbqjkdb qkj bdqksjbd qkdb qkjbd qksjdbqkjbs dqksjdqkjbdqskbqskdbfjgzeg  gzieru zueigrsdfkuog zer gjkwfsjqghsfuqy  quygqsfhvqlkjsbh lzyfluvyffuykvq jfhjvsf jhdfkqjzejyg uyfqukvzef jhhfvqkzevf hjvdhsdfsqz eluqgefgqiuzegf ukjbj sd gdfjgb sdj jskdgb sdjkfsjdbgizeugt ip lgskvkhilsghilgu qlzfifg
+dhkwgbkdbfgdgdf
+dsgdfsiogosdfhgodfighdfisugh ods fsdg f 
+fxghdfhgf
+dhkwgbkdbfgdgdf
+dsgdfsiogosdfhgodfighdfisugh ods fsdg f 
+fxghdfhgf
+dhkwgbkdbfgdgdf
+dsgdfsiogosdfhgodfighdfisugh ods fsdg f 
+fxghdfhgf
+dhkwgbkdbfgdgdf
+dsgdfsiogosdfhgodfighdfisugh ods fsdg f 
+fxghdfhgf
+                        dufghieh tiuthiezugtiezurrt rzt dhkwgbkdbfgdgdf
+                        dsgdfsiogosdfhgodfighdfisugh ods fsdg f 
+                        fxghdfhgf
+                        ze tezouth ezilruth ezrt dhkwgbkdbfgdgdf
+                        dsgdfsiogosdfhgodfighdfisugh ods fsdg f 
+                        fxghdfhgf
+                        et ozemth eilh ilt hhbxklg hselbktg smlgsl dhkwgbkdbfgdgdf
+                        dsgdfsiogosdfhgodfighdfisugh ods fsdg f 
+                        fxghdfhgfmoaetzj;fbdfnhzqfgeilr ze  e ei il diluger 
+                        egieshr gpidusrhg swg esrdhkwgbkdbfgdgdf
+                        dsgdfsiogosdfhgodfighdfisugh ods fsdg f 
+                        fxghdfhgf
+                        mg sùhg se
+                        ùg kesràv soe qr$
+                        l'-'(lg
+                        emt
+                        ^tu 
+                        qsT s Sjs*r=ày u
+                            self.t = 0qkdsbkqjsbdjkqsbdqksjdbqskjdbqskbdqskdjbqskdjbqskjdbqskdjbqksjdbqskdbqksdjbqskjbdqskdbqskdjbqskdjbq kjqbdkj qbdkbqskd bjqdkjbqkd bjqkdsj bqkjdbqjkdb qkj bdqksjbd qkdb qkjbd qksjdbqkjbs dqksjdqkjbdqskbqskdbfjgzeg  gzieru zueigrsdfkuog zer gjkwfsjqghsfuqy  quygqsfhvqlkjsbh lzyfluvyffuykvq jfhjvsf jhdfkqjzejyg uyfqukvzef jhhfvqkzevf hjvdhsdfsqz eluqgefgqiuzegf ukjbj sd gdfjgb sdj jskdgb sdjkfsjdbgizeugt ip lgskvkhilsghilgu qlzfifg
+
+
+
+
+                            dufghieh tiuthiezugtiezurrt rzt 
+                            ze tezouth ezilruth ezrt 
+                            et ozemth eilh ilt hhbxklg hselbktg smlgsl moaetzj;fbdfnhzqfgeilr ze  e ei il diluger 
+                            egieshr gpidusrhg swg esr
+                            mg sùhg se
+                            ùg kesràv soe qr$
+                            l'-'(lg
+                            emt
+                            ^tu 
+                            qsT s Sjs*r=ày u
+                            self.view = 4qkdsbkqjsbdjkqsbdqksjdbqskjdbqskbdqskdjbqskdjbqskjdbqskdjbqksjdbqskdbqksdjbqskjbdqskdbqskdjbqskdjbq kjqbdkj qbdkbqskd bjqdkjbqkd bjqkdsj bqkjdbqjkdb qkj bdqksjbd qkdb qkjbd qksjdbqkjbs dqksjdqkjbdqskbqskdbfjgzeg  gzieru zueigrsdfkuog zer gjkwfsjqghsfuqy  quygqsfhvqlkjsbh lzyfluvyffuykvq jfhjvsf jhdfkqjzejyg uyfqukvzef jhhfvqkzevf hjvdhsdfsqz eluqgefgqiuzegf ukjbj sd gdfjgb sdj jskdgb sdjkfsjdbgizeugt ip lgskvkhilsghilgu qlzfifg
+
+
+
+
+                            dufghieh tiuthiezugtiezurrt rzt 
+                            ze tezouth ezilruth ezrt 
+                            et ozemth eilh ilt hhbxklg hselbktg smlgsl moaetzj;fbdfnhzqfgeilr ze  e ei il diluger 
+                            egieshr gpidusrhg swg esr
+                            mg sùhg se
+                            ùg kesràv soe qr$
+                            l'-'(lg
+                            emt
+                            ^tu 
+                            qsT s Sjs*r=ày u
+qkdsbkqjsbdjkqsbdqksjdbqskjdbqskbdqskdjbqskdjbqskjdbqskdjbqksjdbqskdbqksdjbqskjbdqskdbqskdjbqskdjbq kjqbdkj qbdkbqskd bjqdkjbqkd bjqkdsj bqkjdbqjkdb qkj bdqksjbd qkdb qkjbd qksjdbqkjbs dqksjdqkjbdqskbqskdbfjgzeg  gzieru zueigrsdfkuog zer gjkwfsjqghsfuqy  quygqsfhvqlkjsbh lzyfluvyffuykvq jfhjvsf jhdfkqjzejyg uyfqukvzef jhhfvqkzevf hjvdhsdfsqz eluqgefgqiuzegf ukjbj sd gdfjgb sdj jskdgb sdjkfsjdbgizeugt ip lgskvkhilsghilgu qlzfifg
+
+
+
+
+dufghieh tiuthiezugtiezurrt rzt 
+ze tezouth ezilruth ezrt 
+et ozemth eilh ilt hhbxklg hselbktg smlgsl moaetzj;fbdfnhzqfgeilr ze  e ei il diluger 
+egieshr gpidusrhg swg esr
+mg sùhg se
+ùg kesràv soe qr$
+l'-'(lg
+emt
+^tu 
+qsT s Sjs*r=ày u
+qkdsbkqjsbdjkqsbdqksjdbqskjdbqskbdqskdjbqskdjbqskjdbqskdjbqksjdbqskdbqksdjbqskjbdqskdbqskdjbqskdjbq kjqbdkj qbdkbqskd bjqdkjbqkd bjqkdsj bqkjdbqjkdb qkj bdqksjbd qkdb qkjbd qksjdbqkjbs dqksjdqkjbdqskbqskdbfjgzeg  gzieru zueigrsdfkuog zer gjkwfsjqghsfuqy  quygqsfhvqlkjsbh lzyfluvyffuykvq jfhjvsf jhdfkqjzejyg uyfqukvzef jhhfvqkzevf hjvdhsdfsqz eluqgefgqiuzegf ukjbj sd gdfjgb sdj jskdgb sdjkfsjdbgizeugt ip lgskvkhilsghilgu qlzfifg
+
+
+
+
+dufghieh tiuthiezugtiezurrt rzt 
+ze tezouth ezilruth ezrt 
+et ozemth eilh ilt hhbxklg hselbktg smlgsl moaetzj;fbdfnhzqfgeilr ze  e ei il diluger 
+egieshr gpidusrhg swg esr
+mg sùhg se
+ùg kesràv soe qr$
+l'-'(lg
+emt
+^tu 
+qsT s Sjs*r=ày u
+                elif self.view == 1: #attaqueqkdsbkqjsbdjkqsbdqksjdbqskjdbqskbdqskdjbqskdjbqskjdbqskdjbqksjdbqskdbqksdjbqskjbdqskdbqskdjbqskdjbq kjqbdkj qbdkbqskd bjqdkjbqkd bjqkdsj bqkjdbqjkdb qkj bdqksjbd qkdb qkjbd qksjdbqkjbs dqksjdqkjbdqskbqskdbfjgzeg  gzieru zueigrsdfkuog zer gjkwfsjqghsfuqy  quygqsfhvqlkjsbh lzyfluvyffuykvq jfhjvsf jhdfkqjzejyg uyfqukvzef jhhfvqkzevf hjvdhsdfsqz eluqgefgqiuzegf ukjbj sd gdfjgb sdj jskdgb sdjkfsjdbgizeugt ip lgskvkhilsghilgu qlzfifg
+
+
+
+
+                    dufghieh tiuthiezugtiezurrt rzt 
+                    ze tezouth ezilruth ezrt 
+                    et ozemth eilh ilt hhbxklg hselbktg smlgsl moaetzj;fbdfnhzqfgeilr ze  e ei il diluger 
+                    egieshr gpidusrhg swg esr
+                    mg sùhg se
+                    ùg kesràv soe qr$
+                    l'-'(lg
+                    emt
+                    ^tu 
+                    qsT s Sjs*r=ày u
+                    self.afficher_carte()qkdsbkqjsbdjkqsbdqksjdbqskjdbqskbdqskdjbqskdjbqskjdbqskdjbqksjdbqskdbqksdjbqskjbdqskdbqskdjbqskdjbq kjqbdkj qbdkbqskd bjqdkjbqkd bjqkdsj bqkjdbqjkdb qkj bdqksjbd qkdb qkjbd qksjdbqkjbs dqksjdqkjbdqskbqskdbfjgzeg  gzieru zueigrsdfkuog zer gjkwfsjqghsfuqy  quygqsfhvqlkjsbh lzyfluvyffuykvq jfhjvsf jhdfkqjzejyg uyfqukvzef jhhfvqkzevf hjvdhsdfsqz eluqgefgqiuzegf ukjbj sd gdfjgb sdj jskdgb sdjkfsjdbgizeugt ip lgskvkhilsghilgu qlzfifg
+
+
+
+
+                    dufghieh tiuthiezugtiezurrt rzt 
+                    ze tezouth ezilruth ezrt 
+                    et ozemth eilh ilt hhbxklg hselbktg smlgsl moaetzj;fbdfnhzqfgeilr ze  e ei il diluger 
+                    egieshr gpidusrhg swg esr
+
+                        if event.key == pygame.K_m:qkdsbkqjsbdjkqsbdqksjdbqskjdbqskbdqskdjbqskdjbqskjdbqskdjbqksjdbqskdbqksdjbqskjbdqskdbqskdjbqskdjbq kjqbdkj qbdkbqskd bjqdkjbqkd bjqkdsj bqkjdbqjkdb qkj bdqksjbd qkdb qkjbd qksjdbqkjbs dqksjdqkjbdqskbqskdbfjgzeg  gzieru zueigrsdfkuog zer gjkwfsjqghsfuqy  quygqsfhvqlkjsbh lzyfluvyffuykvq jfhjvsf jhdfkqjzejyg uyfqukvzef jhhfvqkzevf hjvdhsdfsqz eluqgefgqiuzegf ukjbj sd gdfjgb sdj jskdgb sdjkfsjdbgizeugt ip lgskvkhilsghilgu qlzfifg
+
+
+
+
+                        dufghieh tiuthiezugtiezurrt rzt 
+                        ze tezouth ezilruth ezrt 
+                        et ozemth eilh ilt hhbxklg hselbktg smlgsl moaetzj;fbdfnhzqfgeilr ze  e ei il diluger 
+                        egieshr gpidusrhg swg esr
+                        mg sùhg se
+                        ùg kesràv soe qr$
+                        l'-'(lg
+                        emt
+                        ^tu 
+                        qsT s Sjs*r=ày u
+                            self.t = 1qkdsbkqjsbdjkqsbdqksjdbqskjdbqskbdqskdjbqskdjbqskjdbqskdjbqksjdbqskdbqksdjbqskjbdqskdbqskdjbqskdjbq kjqbdkj qbdkbqskd bjqdkjbqkd bjqkdsj bqkjdbqjkdb qkj bdqksjbd qkdb qkjbd qksjdbqkjbs dqksjdqkjbdqskbqskdbfjgzeg  gzieru zueigrsdfkuog zer gjkwfsjqghsfuqy  quygqsfhvqlkjsbh lzyfluvyffuykvq jfhjvsf jhdfkqjzejyg uyfqukvzef jhhfvqkzevf hjvdhsdfsqz eluqgefgqiuzegf ukjbj sd gdfjgb sdj jskdgb sdjkfsjdbgizeugt ip lgskvkhilsghilgu qlzfifg
+
+
+
+
+                            dufghieh tiuthiezugtiezurrt rzt 
+                            ze tezouth ezilruth ezrt 
+                            et ozemth eilh ilt hhbxklg hselbktg smlgsl moaetzj;fbdfnhzqfgeilr ze  e ei il diluger 
+                            egieshr gpidusrhg swg esr
+                            mg sùhg se
+                            ùg kesràv soe qr$
+                            l'-'(lg
+                            emt
+                            ^tu 
+                            qsT s Sjs*r=ày u
+                            self.view = 4qkdsbkqjsbdjkqsbdqksjdbqskjdbqskbdqskdjbqskdjbqskjdbqskdjbqksjdbqskdbqksdjbqskjbdqskdbqskdjbqskdjbq kjqbdkj qbdkbqskd bjqdkjbqkd bjqkdsj bqkjdbqjkdb qkj bdqksjbd qkdb qkjbd qksjdbqkjbs dqksjdqkjbdqskbqskdbfjgzeg  gzieru zueigrsdfkuog zer gjkwfsjqghsfuqy  quygqsfhvqlkjsbh lzyfluvyffuykvq jfhjvsf jhdfkqjzejyg uyfqukvzef jhhfvqkzevf hjvdhsdfsqz eluqgefgqiuzegf ukjbj sd gdfjgb sdj jskdgb sdjkfsjdbgizeugt ip lgskvkhilsghilgu qlzfifg
+
+
+
+
+                            dufghieh tiuthiezugtiezurrt rzt 
+                            ze tezouth ezilruth ezrt 
+                            et ozemth eilh ilt hhbxklg hselbktg smlgsl moaetzj;fbdfnhzqfgeilr ze  e ei il diluger 
+                            egieshr gpidusrhg swg esr
+                            mg sùhg se
+                            ùg kesràv soe qr$
+                            l'-'(lg
+                            emt
+                            ^tu 
+                            qsT s Sjs*r=ày u
+qkdsbkqjsbdjkqsbdqksjdbqskjdbqskbdqskdjbqskdjbqskjdbqskdjbqksjdbqskdbqksdjbqskjbdqskdbqskdjbqskdjbq kjqbdkj qbdkbqskd bjqdkjbqkd bjqkdsj bqkjdbqjkdb qkj bdqksjbd qkdb qkjbd qksjdbqkjbs dqksjdqkjbdqskbqskdbfjgzeg  gzieru zueigrsdfkuog zer gjkwfsjqghsfuqy  quygqsfhvqlkjsbh lzyfluvyffuykvq jfhjvsf jhdfkqjzejyg uyfqukvzef jhhfvqkzevf hjvdhsdfsqz eluqgefgqiuzegf ukjbj sd gdfjgb sdj jskdgb sdjkfsjdbgizeugt ip lgskvkhilsghilgu qlzfifg
+
+
+
+
+dufghieh tiuthiezugtiezurrt rzt 
+ze tezouth ezilruth ezrt 
+et ozemth eilh ilt hhbxklg hselbktg smlgsl moaetzj;fbdfnhzqfgeilr ze  e ei il diluger 
+egieshr gpidusrhg swg esr
+mg sùhg se
+ùg kesràv soe qr$
+l'-'(lg
+emt
+^tu 
+qsT s Sjs*r=ày u
+                elif self.view == 2: #déplacementqkdsbkqjsbdjkqsbdqksjdbqskjdbqskbdqskdjbqskdjbqskjdbqskdjbqksjdbqskdbqksdjbqskjbdqskdbqskdjbqskdjbq kjqbdkj qbdkbqskd bjqdkjbqkd bjqkdsj bqkjdbqjkdb qkj bdqksjbd qkdb qkjbd qksjdbqkjbs dqksjdqkjbdqskbqskdbfjgzeg  gzieru zueigrsdfkuog zer gjkwfsjqghsfuqy  quygqsfhvqlkjsbh lzyfluvyffuykvq jfhjvsf jhdfkqjzejyg uyfqukvzef jhhfvqkzevf hjvdhsdfsqz eluqgefgqiuzegf ukjbj sd gdfjgb sdj jskdgb sdjkfsjdbgizeugt ip lgskvkhilsghilgu qlzfifg
+
+
+
+
+                    dufghieh tiuthiezugtiezurrt rzt 
+                    ze tezouth ezilruth ezrt 
+                    et ozemth eilh ilt hhbxklg hselbktg smlgsl moaetzj;fbdfnhzqfgeilr ze  e ei il diluger 
+                    egieshr gpidusrhg swg esr
+                    mg sùhg se
+                    ùg kesràv soe qr$
+                    l'-'(lg
+                    emt
+                    ^tu 
+                    qsT s Sjs*r=ày u
+                    self.afficher_carte()qkdsbkqjsbdjkqsbdqksjdbqskjdbqskbdqskdjbqskdjbqskjdbqskdjbqksjdbqskdbqksdjbqskjbdqskdbqskdjbqskdjbq kjqbdkj qbdkbqskd bjqdkjbqkd bjqkdsj bqkjdbqjkdb qkj bdqksjbd qkdb qkjbd qksjdbqkjbs dqksjdqkjbdqskbqskdbfjgzeg  gzieru zueigrsdfkuog zer gjkwfsjqghsfuqy  quygqsfhvqlkjsbh lzyfluvyffuykvq jfhjvsf jhdfkqjzejyg uyfqukvzef jhhfvqkzevf hjvdhsdfsqz eluqgefgqiuzegf ukjbj sd gdfjgb sdj jskdgb sdjkfsjdbgizeugt ip lgskvkhilsghilgu qlzfifg
+
+
+
+
+                    dufghieh tiuthiezugtiezurrt rzt 
+                    ze tezouth ezilruth ezrt 
+                    et ozemth eilh ilt hhbxklg hselbktg smlgsl moaetzj;fbdfnhzqfgeilr ze  e ei il diluger 
+                    egieshr gpidusrhg swg esr
+                    mg sùhg se
+                    ùg kesràv soe qr$
+                    l'-'(lg
+                    emt
+                    ^tu 
+                    qsT s Sjs*r=ày u
+                    self.window.blit(self.text_font.render(qkdsbkqjsbdjkqsbdqksjdbqskjdbqskbdqskdjbqskdjbqskjdbqskdjbqksjdbqskdbqksdjbqskjbdqskdbqskdjbqskdjbq kjqbdkj qbdkbqskd bjqdkjbqkd bjqkdsj bqkjdbqjkdb qkj bdqksjbd qkdb qkjbd qksjdbqkjbs dqksjdqkjbdqskbqskdbfjgzeg  gzieru zueigrsdfkuog zer gjkwfsjqghsfuqy  quygqsfhvqlkjsbh lzyfluvyffuykvq jfhjvsf jhdfkqjzejyg uyfqukvzef jhhfvqkzevf hjvdhsdfsqz eluqgefgqiuzegf ukjbj sd gdfjgb sdj jskdgb sdjkfsjdbgizeugt ip lgskvkhilsghilgu qlzfifg
+                    
+                    
+                    
+                    
+                    dufghieh tiuthiezugtiezurrt rzt 

bb07422ed502ca3d19065f4de7146574d4149f3b
AntoinePscl <antoine.pascal03@gmail.com>
2023-05-02 17:30:13 +0200

    correction

--- main.py
+++ main.py
@@ -312,7 +312,7 @@ class MainMenu :
             print(self.OUT)
             if len(self.OUT) == int(self.NbrJoueur.get()):
                 # create the window
-                window_pg = carte_final.PygameWindow((640, 480), self.liste_joueurs)
+                window_pg = carte_final.PygameWindow((640, 480), self.OUT)
 
                 # run the main loop
                 window_pg.main_loop()

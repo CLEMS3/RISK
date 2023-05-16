374fcbf2df9ee2bc9b46e79e73b51112deca7d92
olmetaprogrammer <128503773+olmetaprogrammer@users.noreply.github.com>
2023-05-16 21:41:26 +0200

    sauvegarde nombre de troupes minimum faites

--- carte_final.py
+++ carte_final.py
@@ -486,7 +486,7 @@ class PygameWindow(pygame.Surface):
         self.view = 0 if self.a_qui_le_tour in self.tour_initial else 1
         if self.view == 0 : 
             for territoire in self.game.li_territoires: 
-                self.game.nb_troupes_minimum[territoire]=self.game.li_territoires_obj[territoire].nombre_troupes
+                self.game.nb_troupes_minimum[territoire]=self.game.get_territoire_object(territoire).nombre_troupes
         self.select=[]
 
     def next_player(self):
